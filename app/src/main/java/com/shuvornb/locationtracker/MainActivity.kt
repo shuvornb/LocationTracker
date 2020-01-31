325522ffbda04e83a4375acce96f1c6e356b652f
shuvornb <shuvornb@gmail.com>
2020-01-31 11:28:46 -0500

    last known location displayed

--- app/src/main/java/com/shuvornb/locationtracker/MainActivity.kt
+++ app/src/main/java/com/shuvornb/locationtracker/MainActivity.kt
@@ -1,17 +1,71 @@
 package com.shuvornb.locationtracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity:AppCompatActivity() {
+     private var client: FusedLocationProviderClient? = null
 
-        locBtn.setOnClickListener {
-            txtViewDisplayLoc.text = "Latitude: XXX, Longitude: YYY"
-        }
+     override fun onCreate(savedInstanceState:Bundle?) {
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main)
+
+         requestPermission()
+
+         client = LocationServices.getFusedLocationProviderClient(this)
+         locBtn.setOnClickListener {
+             if (ActivityCompat.checkSelfPermission(this@MainActivity, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                 return@setOnClickListener
+             }
+
+             if (ActivityCompat.checkSelfPermission(this@MainActivity, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                 return@setOnClickListener
+             }
+
+             client!!.lastLocation.addOnSuccessListener(this@MainActivity) { location ->
+                 if (location != null) {
+                     txtViewDisplayLoc.setText(location!!.toString())
+                 }
+                 else {
+                     txtViewDisplayLoc.setText("No data found")
+                 }
+             }
+
+
+             val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+             val criteria = Criteria()
+             val bestProvider = locationManager.getBestProvider(criteria, false)
+             val location = locationManager.getLastKnownLocation(bestProvider!!)
+             try {
+                 val str = location!!.latitude.toString() + " " + location.longitude.toString()
+                 txtViewDisplayLoc.text = str
+             } catch (e: NullPointerException) {
+                 Toast.makeText(this, "No location data found", Toast.LENGTH_SHORT).show()
+             }
+
+
+         }
+
+     }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION),
+            1
+        )
     }
+
 }
